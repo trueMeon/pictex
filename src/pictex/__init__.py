b72a7be5b442e8ad38173ced21753f44297778c9
Franco Zanardi <francozanardi.97@gmail.com>
2026-01-10 17:34:36 -0300

    chore: bump to version 2.0.1

--- src/pictex/__init__.py
+++ src/pictex/__init__.py
@@ -10,7 +10,7 @@ from .models.public import *
 from .bitmap_image import BitmapImage
 from .vector_image import VectorImage
 
-__version__ = "2.0.0"
+__version__ = "2.0.1"
 
 __all__ = [
     "Canvas", "Text", "Row", "Column", "Image", "Element",
